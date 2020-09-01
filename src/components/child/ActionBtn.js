89e4c21c5a63a278b36724fdbee06719d664fe34
sonia <soniamianji1@gmail.com>
2020-09-01 21:28:40 +0200

    add Action btn component

---
+++ src/components/child/ActionBtn.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import arrow from "../../assets/arrow.svg";
+
+const ActionButton = styled.button`
+appearance: none; 
+border: 2px solid ${(props) => props.theme.mainBlue};
+color: ${(props) => props.theme.mainBlue}; 
+padding 0.25em 0.5em;
+transition: background-color 0.25s, color 0.25s;
+box-sizing: border-box;
+border-radius: 5px;
+font-family: ${(props) => props.theme.headerFontFamily};
+font-style: normal;
+font-weight: normal;
+background-color: transparent;
+padding: 1rem;
+font-size: 20px;
+
+&:hover {
+    background-color: ${(props) => props.theme.mainBlue};
+    color: ${(props) => props.theme.regalWhite};
+    cursor: pointer;
+}
+&:hover:after{
+    filter:invert(0) brightness(100);
+}
+&:after{
+    content: url(${arrow});
+    margin-left: 12px;
+}
+`;
+
+const ActionBtn = () => {
+  return <ActionButton>Meddela mig när nästa artikel publiceras</ActionButton>;
+};
+
+export default ActionBtn;
