82a111936cb52a94b4f7794dc2b4fb00ae35b0b1
sonia <soniamianji1@gmail.com>
2020-09-01 21:28:04 +0200

    create raw data file for articles

---
+++ src/rawArticleData.js
@@ -0,0 +1,28 @@
+const articleData = [
+  [
+    {
+      img: require("./assets/square.png"),
+      title: "#1",
+      description: "Samarbete är viktigare än någonsin",
+    },
+    {
+      img: "",
+      title: "Pubiceras v.20",
+      description: "Vad är need to have och vad är bara nice?",
+    },
+  ],
+  [
+    {
+      img: "",
+      title: "Pubiceras v.21",
+      description: "Fastigheterna som bygger samhället",
+    },
+    {
+      img: "",
+      title: "Pubiceras v.22",
+      description: "Vad lär vi oss av krisen?",
+    },
+  ],
+];
+
+export default articleData;
