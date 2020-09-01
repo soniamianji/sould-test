191acbad8c3399e7418136d6aa6130bfe06336aa
sonia <soniamianji1@gmail.com>
2020-09-01 21:29:25 +0200

    create theme component

---
+++ src/config/theme.js
@@ -0,0 +1,7 @@
+export const mainBlue = "#0048b4";
+export const regalGray = "#f4f7fa";
+export const regalBlue = "#004587";
+export const regalWhite = "#fff";
+
+export const headerFontFamily = "Titillium Web ";
+export const bodyFontFamily = " Source Serif Pro ";
