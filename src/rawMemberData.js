cd60775b44fde350de278f15af8354874c0f1a57
sonia <soniamianji1@gmail.com>
2020-09-02 18:17:54 +0200

    add raw data files

---
+++ src/rawMemberData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Lars kjellgren",
+  },
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Peter kjellgren",
+  },
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Stephan Sven",
+  },
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Gustav Sven",
+  },
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Marcus Sven",
+  },
+  {
+    image: require("./assets/portrait.jpg"),
+    name: "Karl Sven",
+  },
+];
---
+++ src/rawOfferingData.js
@@ -0,0 +1,36 @@
+export default [
+  [
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+  ],
+  [
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+    {
+      icon: require("./assets/icon.svg"),
+      text:
+        " Rådgivning och stöd kring ändrad produktion om du ställer om för att exempelvis producera handdesinfektion.",
+    },
+  ],
+];
