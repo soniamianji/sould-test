c68f6ce23b70a2c05a3a4f0734e136ed0f862abe
sonia <soniamianji1@gmail.com>
2020-09-03 10:43:30 +0200

    convert to functional components

--- src/components/child/LeftArrow.js
+++ src/components/child/LeftArrow.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
-class LeftArrow extends Component {
-  render() {
-    return (
-      <div onClick={this.props.goToPrevSlide}>
-        <i
-          className="fa fa-angle-left fa-2x"
-          aria-hidden="true"
-          style={{ color: "#004587" }}
-        ></i>
-      </div>
-    );
-  }
+import React from "react";
+function LeftArrow(props) {
+  return (
+    <div onClick={props.goToPrevSlide}>
+      <i
+        className="fa fa-angle-left fa-2x"
+        aria-hidden="true"
+        style={{ color: "#004587" }}
+      ></i>
+    </div>
+  );
 }
 export default LeftArrow;
--- src/components/child/RightArrow.js
+++ src/components/child/RightArrow.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "font-awesome/css/font-awesome.min.css";
-class RightArrow extends Component {
-  render() {
-    return (
-      <div onClick={this.props.goToNextSlide}>
-        <i
-          className="fa fa-angle-right fa-2x"
-          aria-hidden="true"
-          style={{ color: "#004587" }}
-        ></i>
-      </div>
-    );
-  }
+function RightArrow(props) {
+  return (
+    <div onClick={props.goToNextSlide}>
+      <i
+        className="fa fa-angle-right fa-2x"
+        aria-hidden="true"
+        style={{ color: "#004587" }}
+      ></i>
+    </div>
+  );
 }
+
 export default RightArrow;
